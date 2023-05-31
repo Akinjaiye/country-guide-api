557d46482e841f3b9d9e2f7ae5bd18cfe4d91663
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-05-31 13:05:25 +0100

    add final url in main.js

--- src/main.js
+++ src/main.js
@@ -7,4 +7,7 @@ let searchBtn = document.querySelector('#search-btn');
 let country = document.querySelector('#country');
 searchBtn.addEventListener("click", () =>{
     let countryName = country.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    console.log(finalURL);
+    
 });
