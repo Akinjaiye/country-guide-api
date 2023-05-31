81d45b27bc0da3ed220be2c9a28010f15666c779
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-05-31 12:50:37 +0100

    add variables to ids in main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 
 
+let searchBtn = document.querySelector('#search-btn');
+let country = document.querySelector('#country');
+searchBtn.addEventListener("click", () =>{
+    let countryName = country.value;
+});
